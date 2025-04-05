1a8bb35e530d6f4ec17c3415440ccab25be21a6b
Onur <koseo1@tcnj.edu>
2025-04-05 12:18:37 -0400

    removed comments

--- my_strings.h
+++ my_strings.h
@@ -2,14 +2,14 @@
 
 #ifndef MY_STRINGS_H
 #define MY_STRINGS_H
-#include <stddef.h>    // ! REMOVE DONE COMMENTS WHEN FINISHED
+#include <stddef.h>
 
 char *my_strcat(char *restrict dst, const char *restrict src);
-char *my_strchr(const char *s, int c);    // in prog
-int my_strcmp(const char *s1, const char *s2);    // done
-char *my_strcpy(char *restrict dst, const char *restrict src);    // done
-char *my_strdup(const char *s);    // done
-size_t my_strlen(const char *s);    // done
+char *my_strchr(const char *s, int c);
+int my_strcmp(const char *s1, const char *s2);
+char *my_strcpy(char *restrict dst, const char *restrict src);
+char *my_strdup(const char *s);
+size_t my_strlen(const char *s);
 char *my_strstr(const char *haystack, const char *needle);
 
 #endif
